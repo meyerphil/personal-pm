975c5700c2291adfad1b7694a2d5378aa909eeb3
Phil Meyer <meyerphil3@gmail.com>
2021-07-01 13:59:05 -0700

    script

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 function toggleMenu() {
     var x = document.getElementById("menu");
-	if(x.style.maxHeight === "0px"){
+
+    if(x.style.maxHeight === "0px"){
         x.style.maxHeight = "300px";
-		document.getElementById("mb").innerHTML = "&#10006;";
+	document.getElementById("mb").innerHTML = "&#10006;";
     }else{
         x.style.maxHeight = "0px";
-		document.getElementById("mb").innerHTML = "&#9776;";
+	document.getElementById("mb").innerHTML = "&#9776;";
     }
+
 }
