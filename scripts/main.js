5bf54d9b3c951b00c8bc06b7c1b62a604982323b
Phil Meyer <meyerphil3@gmail.com>
2021-07-01 13:54:43 -0700

    more responsive menu

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,9 @@ function toggleMenu() {
     var x = document.getElementById("menu");
 	if(x.style.maxHeight === "0px"){
         x.style.maxHeight = "300px";
+		document.getElementById("mb").innerHTML = "&#10006;";
     }else{
         x.style.maxHeight = "0px";
+		document.getElementById("mb").innerHTML = "&#9776;";
     }
 }
