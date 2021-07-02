1f34dda31d2d4e35e493f7c5039de589ea4f3d70
Phil Meyer <meyerphil3@gmail.com>
2021-07-01 17:08:28 -0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,10 @@ function toggleMenu() {
 
     if(x.style.maxHeight === "0px"){
         x.style.maxHeight = "300px";
-		document.getElementById("m").innerHTML = "x";
+		document.getElementById("m").innerHTML = "&#10005;";
     }else{
         x.style.maxHeight = "0px";
+        document.getElementById("m").innerHTML = "&#9776;";
     }
 
 }
