4982d236e14be2f6563ee2294b7b72f06baa75a7
Phil Meyer III <philmeyer@Phils-Air.attlocal.net>
2025-02-12 23:56:39 -0800

    updated slide scaling

--- scripts/loadprojectpage.js
+++ scripts/loadprojectpage.js
@@ -73,7 +73,7 @@ function createProjectHTML(project) {
             } else if (info.type === 'slides') {
                 moreinfoHTML += `<p class="text">
                                     <b>${info.header}</b>
-                                    <br><iframe src="${info.src}/embed?start=false&loop=true&delayms=5000" frameborder="0" width="685" height="422" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                                    <br><iframe class = "slides" src="${info.src}/embed?start=false&loop=true&delayms=5000" frameborder="0" width="685" height="422" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                                     
                                  </p>`;
             } else if (info.type === 'video') {
